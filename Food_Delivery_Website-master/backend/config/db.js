09974540f590de24eeae89f971252d2b67376b3d
Shylaja Gajvinkar <shylaja2006@gmail.com>
2026-04-28 00:13:20 +0530

    fix mongo connection

--- Food_Delivery_Website-master/backend/config/db.js
+++ Food_Delivery_Website-master/backend/config/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-export const  connectDB = async () =>{
-
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/food_delivery').then(()=>console.log("DB Connected"));
-   
-}
-
-
-// add your mongoDB connection string in .env file as MONGODB_URI
-// Do not use '@' symbol in your database user's password else it will show an error.
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB Connected");
+  } catch (error) {
+    console.log("DB Error:", error.message);
+    process.exit(1);
+  }
+};
