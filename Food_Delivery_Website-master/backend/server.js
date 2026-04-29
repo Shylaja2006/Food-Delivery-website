94c3720c15d8f7337653dc3924cc01bac5348f4f
Shylaja Gajvinkar <shylaja2006@gmail.com>
2026-04-29 15:04:19 +0530

    fix uploads path for render

--- Food_Delivery_Website-master/backend/server.js
+++ Food_Delivery_Website-master/backend/server.js
@@ -27,7 +27,7 @@ connectDB()
 // api endpoints
 app.use("/api/user", userRouter)
 app.use("/api/food", foodRouter)
-app.use("/images",express.static('uploads'))
+app.use("/images",express.static(Path2D.join(process.cwd(),"uploads")));
 app.use("/api/cart", cartRouter)
 app.use("/api/order",orderRouter)
 
